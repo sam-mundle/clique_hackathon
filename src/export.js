7bbdba358cc9957f2b5b431a7097ca760a728936
Sam Mundle <sam.mundle13@gmail.com>
2017-10-03 12:45:39 -0400

    exportTemplate

---
+++ src/export.js
@@ -0,0 +1,298 @@
+export default [
+
+	// Slide 2
+    {
+        question: 'What\'s your living situation?',
+		answer: [
+			{
+				answer: 'condo',
+				image_url: './image/slide_2_living_situation/condo.jpg'
+			},
+			{
+			    answer: 'digital_nomad',
+			    image_url: './image/slide_2_living_situation/digital_nomad.jpg'
+			},
+			{
+			    answer: 'dorm',
+			    image_url: './image/slide_2_living_situation/dorm.jpg'
+			},
+            {
+                answer: 'home',
+                image_url: './image/slide_2_living_situation/home.jpg'
+            }
+			
+		]
+    },
+    // Slide 3
+    {
+        question: 'What do you wear to work?',
+        answer: [
+			{
+			    answer: 'business formal',
+			    image_url: './image/slide_3_work_wear/business_formal'
+			},
+			{
+			    answer: 'business casual',
+			    image_url: './image/slide_3_work_wear/business_casual'
+			},
+			{
+			    answer: 'fashion girl',
+			    image_url: './image/slide_3_work_wear/fashion_girl'
+			},
+			{
+			    answer: 'creative/artistic',
+			    image_url: './image/slide_3_work_wear/creative'
+			},
+            {
+                answer: 'start-up',
+                image_url: './image/slide_3_work_wear/startup'
+            },
+            {
+                answer: 'work from home',
+                image_url: './image/slide_3_work_wear/work_from_home'
+            }
+			
+        ]
+    },
+    // Slide 4
+    {
+        question: 'Whose personal style are you drawn to?',
+        answer: [
+			{
+			    answer: 'classic minimal',
+			    image_url: './image/slide_4_influencer_inspiration/Classic_minimal'
+			},
+			{
+			    answer: 'edgy',
+			    image_url: './image/slide_4_influencer_inspiration/Edgy'
+			},
+			{
+			    answer: 'feminine',
+			    image_url: './image/slide_4_influencer_inspiration/Feminine'
+			},
+			{
+			    answer: 'maximalist',
+			    image_url: './image/slide_4_influencer_inspiration/Maximalist'
+			},
+            {
+                answer: 'trendy',
+                image_url: './image/slide_4_influencer_inspiration/Trendy'
+            }
+			
+        ]
+    },
+    // Slide 5
+    {
+        question: 'Describe your skincare routine',
+        answer: [
+			{
+			    answer: 'cleanse tone oil moisturize',
+			    image_url: './image/slide_5_skincare/Cleanse_tone_oil_moisturize'
+			},
+			{
+			    answer: 'like saphora',
+			    image_url: './image/slide_5_skincare/sophora'
+			},
+			{
+			    answer: 'use serum',
+			    image_url: './image/slide_5_skincare/serum'
+			},
+			{
+			    answer: 'wash my face',
+			    image_url: './image/slide_5_skincare/wash_my_face'
+			}
+			
+        ]
+    },
+    // Slide 6
+    {
+        question: 'It\'s Friday night. What are you up to?',
+        answer: [
+			{
+			    answer: 'family time',
+			    image_url: './image/slide_6_friday_night/FamilyTime'
+			},
+			{
+			    answer: 'girls night',
+			    image_url: './image/slide_6_friday_night/GirlsNight'
+			},
+			{
+			    answer: 'takeout netflix and chill',
+			    image_url: './image/slide_6_friday_night/TakeoutNetflixChill'
+			}
+			
+        ]
+    },
+    // Slide 7
+    {
+        question: 'What does your dream living room look like?',
+        answer: [
+			{
+			    answer: 'coastal',
+			    image_url: './image/slide_7_dream_livingroom/Coastal'
+			},
+			{
+			    answer: 'eclectic',
+			    image_url: './image/slide_7_dream_livingroom/Eclectic'
+			},
+			{
+			    answer: 'maximal',
+			    image_url: './image/slide_7_dream_livingroom/Maximal'
+			},
+			{
+			    answer: 'midcentury',
+			    image_url: './image/slide_7_dream_livingroom/Midcentury'
+			},
+            {
+                answer: 'minimal',
+                image_url: './image/slide_7_dream_livingroom/Minimal'
+            },
+            {
+                answer: 'transitional',
+                image_url: './image/slide_7_dream_livingroom/Transitional'
+            }
+			
+        ]
+    },
+    // Slide 8
+    {
+        question: 'You have free time. What are you doing?',
+        answer: [
+			{
+			    answer: 'cooking',
+			    image_url: './image/slide_8_freetime/Cooking'
+			},
+			{
+			    answer: 'reading',
+			    image_url: './image/slide_8_freetime/ReadingBook'
+			},
+			{
+			    answer: 'shopping',
+			    image_url: './image/slide_8_freetime/Shopping'
+			},
+			{
+			    answer: 'travel',
+			    image_url: 'Travel'
+			},
+            {
+                answer: 'wellness selfcare',
+                image_url: 'WellnessSelfCare'
+            },
+            {
+                answer: 'working out',
+                image_url: 'WorkingOut'
+            }
+			
+        ]
+    },
+    // Slide 2
+    {
+        question: '',
+        answer: [
+			{
+			    answer: '',
+			    image_url: ''
+			},
+			{
+			    answer: '',
+			    image_url: ''
+			},
+			{
+			    answer: '',
+			    image_url: ''
+			},
+			{
+			    answer: '',
+			    image_url: ''
+			}
+			
+        ]
+    },
+    // Slide 2
+    {
+        question: '',
+        answer: [
+			{
+			    answer: '',
+			    image_url: ''
+			},
+			{
+			    answer: '',
+			    image_url: ''
+			},
+			{
+			    answer: '',
+			    image_url: ''
+			},
+			{
+			    answer: '',
+			    image_url: ''
+			}
+			
+        ]
+    },
+	
+	
+	
+	
+	
+	
+	
+    {
+        image_url: 'https://scontent-lax3-1.cdninstagram.com/t51.2885-15/e35/20687913_1946136592340698_7941965000745156608_n.jpg',
+        image_credit: '@arginehdanelian',
+        image_credit_url: 'https://www.instagram.com/arginehdanelian/',
+        products: [
+            'mTYnDq',
+            'mTYnTR'
+        ]
+    },
+    {
+        image_url: 'https://scontent-atl3-1.cdninstagram.com/t51.2885-15/e35/21296949_1546342225388851_9128232412394815488_n.jpg',
+        image_credit: '@melinchanel',
+        image_credit_url: 'https://www.instagram.com/melinchanel/',
+        products: [
+            'mTYnDl'
+        ]
+    },
+    {
+        image_url: 'https://scontent-lax3-1.cdninstagram.com/t51.2885-15/e35/20633309_254263638395602_4224895368422227968_n.jpg',
+        image_credit: '@houseandhanger',
+        image_credit_url: 'https://www.instagram.com/houseandhanger/?hl=en',
+        products: [
+            'mTYnH3'
+        ]
+    },
+    {
+        image_url: 'https://scontent-lax3-1.cdninstagram.com/t51.2885-15/e35/20181240_777585832413921_8259905448094728192_n.jpg',
+        image_credit: '@katherinepower',
+        image_credit_url: 'https://www.instagram.com/katherinepower/?hl=en',
+        products: [
+            'mTBtwH'
+        ]
+    },
+    {
+        image_url: 'https://scontent-atl3-1.cdninstagram.com/t51.2885-15/e35/21294354_288583981619832_535308591876276224_n.jpg',
+        image_credit: '@courtneeruthie',
+        image_credit_url: 'https://www.instagram.com/courtneeruthie/',
+        products: [
+            'mTdjM3'
+        ]
+    },
+    {
+        image_url: 'https://scontent-lax3-1.cdninstagram.com/t51.2885-15/e35/20482363_1390747111046996_3415462795276189696_n.jpg',
+        image_credit: '@mydaywithj',
+        image_credit_url: 'https://www.instagram.com/mydaywithj/',
+        products: [
+            'mTYnTr'
+        ]
+    },
+    {
+        image_url: 'https://scontent-lax3-1.cdninstagram.com/t51.2885-15/e35/20225505_1433582506723586_2255083629715652608_n.jpg',
+        image_credit: '@skirtsofurban',
+        image_credit_url: 'https://www.instagram.com/skirtsofurban/',
+        products: [
+            'mTYn2f'
+        ]
+    }
+];
